d0958002194cca2e7b8fc81a353158889e4d5641
Dean Shi <dean.xiaoshi@gmail.com>
2015-06-01 17:12:36 -0700

    Added LongestSubstringWithoutRepeatingCharacters

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ LeetCode
 |---| ----- | -------- | ---------- |
 |1|[Two Sum](https://oj.leetcode.com/problems/two-sum/)| [JavaScript](./algorithms/TwoSum.js)|Medium|
 |2|[Add Two Numbers](https://oj.leetcode.com/problems/add-two-numbers/)| [JavaScript](./algorithms/AddTwoNumbers.js)|Medium|
+|3|[Longest Substring Without Repeating Characters](https://oj.leetcode.com/problems/longest-substring-without-repeating-characters/)| [JavaScript](./algorithms/LongestSubstringWithoutRepeatingCharacters.js)|Medium|
 |88|[Merge Sorted Array](https://oj.leetcode.com/problems/merge-sorted-array/)| [JavaScript](./algorithms/MergeTwoSortedArray.js)|Easy|
 |102|[Binary Tree Level Order Traversal](https://oj.leetcode.com/problems/binary-tree-level-order-traversal/)| [JavaScript](./algorithms/BinaryTreeLevelOrderTraversal.js)|Easy|
 |136|[Single Number](https://oj.leetcode.com/problems/single-number/)| [JavaScript](./algorithms/SingleNumber.js)|Medium|
---
+++ algorithms/LongestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,40 @@
+// Source : https://oj.leetcode.com/problems/longest-substring-without-repeating-characters/
+// Author : Xiao Shi
+// Date   : 2015-06-01
+
+/**********************************************************************************
+ *
+ * Given a string, find the length of the longest substring without repeating characters.
+ * For example, the longest substring without repeating letters for "abcabcbb" is "abc",
+ * which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+ *
+ **********************************************************************************/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    var max = 0;
+    var last = -1;
+    var map = {};
+
+    for (var i = 0, l = s.length; i < l; ++i) {
+        if (map[s[i]] >= 0 && last < map[s[i]]) {
+            last = map[s[i]];
+        }
+
+        if (i - last > max) {
+            max = i - last;
+        }
+
+        map[s[i]] = i;
+    }
+    return max;
+};
+
+var testCase1 = 'abcabcbb';
+var testCase2 = 'bbbbb';
+
+console.log(lengthOfLongestSubstring(testCase1) === 3);
+console.log(lengthOfLongestSubstring(testCase2) === 1);
