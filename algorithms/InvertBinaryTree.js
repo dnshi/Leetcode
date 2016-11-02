cfb3555a5d555ae2588ea9a41eb406a3387e5536
Dean Shi <dnshi@users.noreply.github.com>
2016-11-02 10:46:46 -0700

    Update InvertBinaryTree.js

--- algorithms/InvertBinaryTree.js
+++ algorithms/InvertBinaryTree.js
@@ -38,37 +38,9 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var invertTree_recursive1 = function(root) {
-    root && invert(root);
-    return root;
-
-    function invert(node) {
-        var tmp = node.left;
-        (node.left = node.right) && invert(node.left);
-        (node.right = tmp) && invert(node.right);
-    }
-};
-
-/**
- * @param {TreeNode} root
- * @return {TreeNode}
- */
-var invertTree_recursive2 = function(root) {
-    if (!root) {
-        return root;
-    }
-    var node = invertTree(root.left);
-    root.left = invertTree(root.right);
-    root.right = node;
-    return root;
-};
-
-/**
- * Main function
- *
- * @param {TreeNode} root
- * @return {TreeNode}
- */
 var invertTree = function(root) {
-    return new Date().getSeconds() % 2 ? invertTree_recursive1(root) : invertTree_recursive2(root);
+    if (root) {
+        [root.left, root.right] = [invertTree(root.right), invertTree(root.left)]   
+    }
+    return root
 };
