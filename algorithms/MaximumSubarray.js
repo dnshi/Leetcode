88be063380572574cdbf958d2f04db3a2e66e0ed
Dean Shi <dean.xiaoshi@gmail.com>
2017-03-02 22:20:30 -0800

    Update MaximumSubarray

--- algorithms/MaximumSubarray.js
+++ algorithms/MaximumSubarray.js
@@ -27,12 +27,11 @@
 var maxSubArray = function(nums) {
     if (!nums.length) return -Infinity
 
-    const dp = [nums[0]]
     let largestSum = nums[0]
-
+    let sum = nums[0]
     for (let i = 1; i < nums.length; i++) {
-        dp[i] = Math.max(dp[i - 1] + nums[i], nums[i])
-        largestSum = Math.max(largestSum, dp[i])
+        sum = Math.max(sum + nums[i], nums[i])
+        largestSum = Math.max(largestSum, sum)
     }
     return largestSum
 };
