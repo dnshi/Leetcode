3c6338e28196dc6674decc7ec1eab81d3dd8f4c6
Dean Shi <dnshi@users.noreply.github.com>
2017-06-21 17:06:37 -0700

    Update FizzBuzz.js

--- algorithms/FizzBuzz.js
+++ algorithms/FizzBuzz.js
@@ -4,8 +4,7 @@
 
 /***************************************************************************************
  *
- *  Write a program that outputs the string representation of numbers from 1 to
- * n.
+ * Write a program that outputs the string representation of numbers from 1 to n.
  *
  * But for multiples of three it should output “Fizz” instead of the number and for the
  * multiples of five output “Buzz”. For numbers which are multiples of both three and
