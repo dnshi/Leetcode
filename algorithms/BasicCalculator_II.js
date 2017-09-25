b7481f7c418d2ceb281728c800b6a36b11aa5d8e
Dean Shi <dean.xiaoshi@gmail.com>
2017-09-25 14:10:40 -0700

    Update BasicCalculator_II

--- algorithms/BasicCalculator_II.js
+++ algorithms/BasicCalculator_II.js
@@ -3,21 +3,22 @@
 // Date   : 2017-09-25
 
 /***************************************************************************************
- * Implement a basic calculator to evaluate a simple expression string
+ *
+ * Implement a basic calculator to evaluate a simple expression string.
  *
  * The expression string contains only non-negative integers, +, -, *, / operators and
- * empty spaces  . The integer division should truncate toward zero
+ * empty spaces  . The integer division should truncate toward zero.
  *
- * You may assume that the given expression is always valid
+ * You may assume that the given expression is always valid.
  *
- * Some examples
+ * Some examples:
  * "3+2*2" = 7
  * " 3/2 " = 1
  * " 3+5 / 2 " = 5
  *
- * Note: Do not use the eval built-in library function
+ * Note: Do not use the eval built-in library function.
  *
- * Credits:Special thanks to @ts for adding this problem and creating all test cases
+ * Credits:Special thanks to @ts for adding this problem and creating all test cases.
  *
  ***************************************************************************************/
 
