3182be2660a1507cfdd372ce16cd4f53987abba4
Dean Shi <dean.xiaoshi@gmail.com>
2017-09-25 14:09:40 -0700

    Update ValidParenthesisString

--- algorithms/ValidParenthesisString.js
+++ algorithms/ValidParenthesisString.js
@@ -6,30 +6,30 @@
  * 
  * Given a string containing only three types of characters: '(', ')' and '*', write a
  * function to check whether this string is valid. We define the validity of a string
- * by these rules
+ * by these rules:
  * 
- * Any left parenthesis '(' must have a corresponding right parenthesis
- * Any right parenthesis ')' must have a corresponding left parenthesis
- * Left parenthesis '(' must go before the corresponding right parenthesis
- * '*' could be treated as a single right parenthesis ')' or a single left parenthesis
- * '(' or an empty string
- * An empty string is also valid
+ * Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+ * Any right parenthesis ')' must have a corresponding left parenthesis '('.
+ * Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+ * '*' could be treated as a single right parenthesis ')' or a single left parenthesis '
+ * (' or an empty string.
+ * An empty string is also valid.
  * 
- * Example 1
- * Input
+ * Example 1:
+ * Input: "()"
  * Output: True
  * 
- * Example 2
- * Input
+ * Example 2:
+ * Input: "(*)"
  * Output: True
  * 
- * Example 3
- * Input
+ * Example 3:
+ * Input: "(*))"
  * Output: True
  * 
- * Note
+ * Note:
  * 
- * The string size will be in the range [1, 100
+ * The string size will be in the range [1, 100].
  * 
  ***************************************************************************************/
 
