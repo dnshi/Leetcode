75b9d7c6c53418d156d841209273648a0bbcb00a
Dean Shi <dean.xiaoshi@gmail.com>
2017-10-23 01:32:07 -0700

    Update 24Game

--- algorithms/24Game.js
+++ algorithms/24Game.js
@@ -3,22 +3,21 @@
 // Date   : 2017-10-23
 
 /***************************************************************************************
- * 
- * 
+ *
  * You have 4 cards each containing a number from 1 to 9.  You need to judge whether
  * they could operated through *, /, +, -, (, ) to get the value of 24.
- * 
+ *
  * Example 1:
  * Input: [4, 1, 8, 7]
  * Output: True
  * Explanation: (8-4) * (7-1) = 24
- * 
+ *
  * Example 2:
  * Input: [1, 2, 1, 2]
  * Output: False
- * 
+ *
  * Note:
- * 
+ *
  * The division operator / represents real division, not integer division.  For example,
  * 4 / (1 - 2/3) = 12.
  * Every operation done is between two numbers.  In particular, we cannot use - as a
@@ -26,8 +25,7 @@
  * 1 is not allowed.
  * You cannot concatenate numbers together.  For example, if the input is [1, 2, 1, 2],
  * we cannot write this as 12 + 12.
- * 
- * 
+ *
  ***************************************************************************************/
 
 /**
