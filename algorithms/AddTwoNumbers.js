313ebd14e30eddaa881537614dab6227065d111f
Dean Shi <dnshi@users.noreply.github.com>
2018-07-23 22:39:33 -0700

    Update AddTwoNumbers.js

--- algorithms/AddTwoNumbers.js
+++ algorithms/AddTwoNumbers.js
@@ -25,85 +25,25 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-// Solution 1:
-// Runtime: 276 ms
 var addTwoNumbers = function(l1, l2) {
-    var arr = [];
-    (l1 || l2) && f(arr, l1, l2, 0);
-    return arr;
-};
-
-function f(arr, l1, l2, n) {
-    if (!l1 && !l2) {
-        n && arr.push(n);
-        return;
-    }
-    var sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + n;
-    var m = ~~(sum / 10); // OR (sum / 10) >> 0 OR (sum / 10) | 0
-
-    arr.push(sum % 10);
-    f(arr, l1 && l1.next, l2 && l2.next, m);
-}
-
-// Solution 2:
-// Runtime: 272
-// var addTwoNumbers = function(l1, l2) {
-//     var sum = l1.val + l2.val;
-//     var next1 = l1.next;
-//     var next2 = l2.next;
-//     var result = new ListNode(sum % 10);
-//     var node = result;
-//     sum = ~~(sum / 10);
-
-//     while (next1 || next2 || sum !== 0) {
-//         if (!next1) {
-//             next1 = {
-//                 val: 0,
-//                 next: null
-//             };
-//         }
-//         if (!next2) {
-//             next2 = {
-//                 val: 0,
-//                 next: null
-//             };
-//         }
-//         sum += next1.val + next2.val;
-//         next1 = next1.next;
-//         next2 = next2.next;
-//         node.next = new ListNode(sum % 10);
-//         node = node.next;
-//         sum = ~~(sum / 10);
-//     }
-//     return result;
-// };
-
-// Test Case
-// ************ Note ************
-// It only works for 1st solution
-// ******************************
-var l1 = {
-    val: 2,
-    next: {
-        val: 4,
-        next: {
-            val: 3,
-            next: null
+    const result = new ListNode(0)
+    let curr = result, carry = 0
+    
+    while (l1 || l2) {
+        if (l1) {
+            carry += l1.val
+            l1 = l1.next
         }
-    }
-};
-
-var l2 = {
-    val: 5,
-    next: {
-        val: 6,
-        next: {
-            val: 4,
-            next: null
+        
+        if (l2) {
+            carry += l2.val
+            l2 = l2.next
         }
+        
+        curr = curr.next = new ListNode(carry % 10)
+        carry = ~~(carry / 10)
     }
+    
+    if (carry !== 0) curr.next = new ListNode(carry)
+    return result.next
 };
-
-var result = addTwoNumbers(l1, l2);
-
-console.log(result.toString() === '7,0,8'); // true
