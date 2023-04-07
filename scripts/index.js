d27c518d16ab1f3e2f854978046ce9e92fcb23fb
Dean Shi <dean.xiaoshi@gmail.com>
2023-04-07 15:21:05 -0700

    Upgrade scripts

--- scripts/index.js
+++ scripts/index.js
@@ -1,139 +1,131 @@
-import fs from 'fs'
-import { promisify } from 'util'
-import puppeteer from 'puppeteer'
-import ReadMe from './readme.js'
-import nodePackage from '../package.json'
+import puppeteer from "puppeteer";
+import ReadMe from "./readme.js";
+import { readFile, writeFile } from "node:fs/promises";
 
-const readFileAsync = promisify(fs.readFile)
-const writeFileAsync = promisify(fs.writeFile)
+const fileUrl = new URL("../package.json", import.meta.url);
+const nodePackage = JSON.parse(await readFile(fileUrl, "utf8"));
 
-const sources = process.argv.slice(2)
-const readme = 'README.md'
+const sources = process.argv.slice(2);
+const readme = "README.md";
 
 class File {
   constructor(sourceFilePath, readmeFilePath) {
-    this.sourceFilePath = sourceFilePath
-    this.readmeFilePath = readmeFilePath
+    this.sourceFilePath = sourceFilePath;
+    this.readmeFilePath = readmeFilePath;
   }
 
   async process() {
-    const success = await this.processSourceFile()
+    const success = await this.processSourceFile();
 
     if (success) {
       await new ReadMe(
         this.sourceFilePath,
         this.readmeFilePath,
-        this.url,
-      ).process()
+        this.url
+      ).process();
     }
   }
 
   async processSourceFile() {
     try {
-      const { sourceFilePath } = this
-      const file = await readFileAsync(sourceFilePath, { encoding: 'utf8' })
+      const { sourceFilePath } = this;
+      const file = await readFile(sourceFilePath, { encoding: "utf8" });
 
-      ;[this.url, ...this.paragraphs] = file.split('\n')
+      [this.url, ...this.paragraphs] = file.split("\n");
       if (!this.isValidUrl(this.url)) {
-        console.log(`${sourceFilePath} has a comment header already`)
-        return false
+        console.log(`${sourceFilePath} has a comment header already`);
+        return false;
       }
-      await this.fetchAll(this.url)
-      await writeFileAsync(sourceFilePath, this.createSourceFile())
+      await this.fetchAll(this.url);
+      await writeFile(sourceFilePath, this.createSourceFile());
 
-      console.log('[Script] Source file has been updated!')
-      return true
+      console.log("[Script] Source file has been updated!");
+      return true;
     } catch (err) {
-      console.error('ERROR:', err)
-      return false
+      console.error("ERROR:", err);
+      return false;
     }
   }
 
   async fetchAll(url) {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto(url)
-    await page.waitForSelector(
-      'div[class^="description"] > div[class^="content"] > div > p',
-    )
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
 
-    this.description = await this.fetchDescription(page)
+    this.description = await this.fetchDescription(page);
 
-    browser.close()
+    browser.close();
   }
 
   async fetchDescription(page) {
-    return page.$eval(
-      'div[class^="description"] > div[class^="content"]',
-      (e) => e.textContent,
-    )
+    return page.$eval("meta[name=description]", (e) => e.content);
   }
 
   getSource() {
-    const suffixes = ['/description', '/discuss', '/submissions']
+    const suffixes = ["/description", "/discuss", "/submissions"];
     return suffixes.some((suffix) => this.url.includes(suffix))
       ? this.url.slice(
           0,
-          Math.max(...suffixes.map((suffix) => this.url.indexOf(suffix))),
+          Math.max(...suffixes.map((suffix) => this.url.indexOf(suffix)))
         )
-      : this.url.replace(/\/$/, '')
+      : this.url.replace(/\/$/, "");
   }
 
   getAuthor() {
-    return nodePackage.author
+    return nodePackage.author;
   }
 
   getDate() {
-    return new Date().toISOString().split('T')[0]
+    return new Date().toISOString().split("T")[0];
   }
 
   getExtension() {
-    return this.sourceFilePath.slice(this.sourceFilePath.lastIndexOf('.') + 1)
+    return this.sourceFilePath.slice(this.sourceFilePath.lastIndexOf(".") + 1);
   }
 
   getDescription() {
-    return this.description
+    return this.description;
   }
 
   getContent(description) {
     return description
-      .replace(/\n\s*\n/g, '\n\n')
-      .split('\n')
+      .replace(/\n\s*\n/g, "\n\n")
+      .split("\n")
       .reduce(
         (lines, line) => {
-          const lineArr = line.match(/.{1,84}($|\W)/g)
+          const lineArr = line.match(/.{1,84}($|\W)/g);
 
           return lineArr
             ? lines.concat(lineArr.map((l) => l.trim()))
-            : lines.concat(line)
+            : lines.concat(line);
         },
-        [''],
-      )
+        [""]
+      );
   }
 
   createHeader(source, author, date) {
-    const extension = this.getExtension()
+    const extension = this.getExtension();
     const [headerSource, headerAuthor, headerDate] = [
-      `${extension === 'js' ? '//' : '#'} Source : ${source}`,
-      `${extension === 'js' ? '//' : '#'} Author : ${author}`,
-      `${extension === 'js' ? '//' : '#'} Date   : ${date}`,
-    ]
+      `${extension === "js" ? "//" : "#"} Source : ${source}`,
+      `${extension === "js" ? "//" : "#"} Author : ${author}`,
+      `${extension === "js" ? "//" : "#"} Date   : ${date}`,
+    ];
 
-    return `${headerSource}\n${headerAuthor}\n${headerDate}\n`
+    return `${headerSource}\n${headerAuthor}\n${headerDate}\n`;
   }
 
   createDescription(description) {
-    const extension = this.getExtension()
+    const extension = this.getExtension();
     const [commentHeader, commentBlock, commentFooter] = [
-      extension === 'js' ? '/'.padEnd(88, '*') : '#'.repeat(82),
-      extension === 'js' ? ' * ' : '#  ',
-      extension === 'js' ? ` ${'/'.padStart(88, '*')}` : '#'.repeat(82),
-    ]
+      extension === "js" ? "/".padEnd(88, "*") : "#".repeat(82),
+      extension === "js" ? " * " : "#  ",
+      extension === "js" ? ` ${"/".padStart(88, "*")}` : "#".repeat(82),
+    ];
 
-    const commentBody = this.getContent(description)
+    const commentBody = this.getContent(description);
     return `${commentHeader}${commentBody.join(
-      `\n${commentBlock}`,
-    )}\n${commentFooter}`
+      `\n${commentBlock}`
+    )}\n${commentFooter}`;
   }
 
   createSourceFile() {
@@ -141,14 +133,14 @@ class File {
       this.createHeader(this.getSource(), this.getAuthor(), this.getDate()),
       this.createDescription(this.getDescription()),
       ...this.paragraphs,
-    ].join('\n')
+    ].join("\n");
   }
 
   isValidUrl(url) {
-    return url.startsWith('http')
+    return url.startsWith("http");
   }
 }
 
 if (sources.length > 0) {
-  sources.map((sourceFilePath) => new File(sourceFilePath, readme).process())
+  sources.map((sourceFilePath) => new File(sourceFilePath, readme).process());
 }
--- scripts/readme.js
+++ scripts/readme.js
@@ -1,134 +1,130 @@
-import https from 'https'
-import fs from 'fs'
-import { promisify } from 'util'
+import https from "https";
+import { readFile, writeFile } from "node:fs/promises";
 
-const readFileAsync = promisify(fs.readFile)
-const writeFileAsync = promisify(fs.writeFile)
-
-const URL = 'https://leetcode.com/api/problems/all/'
+const URL = "https://leetcode.com/api/problems/all/";
 const LANG = {
-  js: 'JavaScript',
-  sh: 'Shell',
-  sql: 'SQL',
-}
+  js: "JavaScript",
+  sh: "Shell",
+  sql: "SQL",
+};
 
 const LEVEL = {
-  1: 'Easy',
-  2: 'Medium',
-  3: 'Hard',
-}
+  1: "Easy",
+  2: "Medium",
+  3: "Hard",
+};
 
 export default class ReadMe {
   constructor(sourceFilePath, readmeFilePath, url) {
-    this.sourceFilePath = sourceFilePath
-    this.readmeFilePath = readmeFilePath
-    this.url = url
+    this.sourceFilePath = sourceFilePath;
+    this.readmeFilePath = readmeFilePath;
+    this.url = url;
   }
 
   async process() {
-    const { stat_status_pairs: data } = await this.fetch(URL)
+    const { stat_status_pairs: data } = await this.fetch(URL);
 
-    const slug = this.getSource().split('/').pop()
+    const slug = this.getSource().split("/").pop();
     const questionInfo = data.find(
-      ({ stat }) => stat.question__title_slug === slug,
-    )
-    const { frontend_question_id, question__title } = questionInfo.stat
-    const { level } = questionInfo.difficulty
+      ({ stat }) => stat.question__title_slug === slug
+    );
+    const { frontend_question_id, question__title } = questionInfo.stat;
+    const { level } = questionInfo.difficulty;
 
-    this.title = `${frontend_question_id}.${question__title}`
-    this.difficulty = LEVEL[level]
+    this.title = `${frontend_question_id}.${question__title}`;
+    this.difficulty = LEVEL[level];
 
-    await this.processReadmeFile()
+    await this.processReadmeFile();
   }
 
   async processReadmeFile() {
     try {
-      const { readmeFilePath } = this
-      const file = await readFileAsync(readmeFilePath, { encoding: 'utf8' })
-      const line = this.searchLine(file)
-      const data = this.insertLine(file, this.getLine(), file.indexOf(line))
-      await writeFileAsync(readmeFilePath, data)
+      const { readmeFilePath } = this;
+      const file = await readFile(readmeFilePath, { encoding: "utf8" });
+      const line = this.searchLine(file);
+      const data = this.insertLine(file, this.getLine(), file.indexOf(line));
+      await writeFile(readmeFilePath, data);
 
-      console.log('[Script] README has been updated!')
+      console.log("[Script] README has been updated!");
     } catch (err) {
-      console.error('ERROR:', err)
+      console.error("ERROR:", err);
     }
   }
 
   searchLine(file) {
-    const regex = new RegExp(`\\|\\d+.*\\[${this.getLanguage()}\\]`, 'g')
-    const lines = file.match(regex)
-    const targetNum = this.getNum()
+    const regex = new RegExp(`\\|\\d+.*\\[${this.getLanguage()}\\]`, "g");
+    const lines = file.match(regex);
+    const targetNum = this.getNum();
 
-    return lines.find((line) => +line.match(/\d+/)[0] < targetNum)
+    return lines.find((line) => +line.match(/\d+/)[0] < targetNum);
   }
 
   insertLine(file, line, index) {
-    return file.slice(0, index) + line + file.slice(index)
+    return file.slice(0, index) + line + file.slice(index);
   }
 
   getLine() {
-    const num = this.getNum()
-    const title = this.getTitle()
-    const source = this.getSource()
-    const language = this.getLanguage()
-    const sourceFilePath = this.getSourceFilePath()
-    const difficulty = this.getDifficulty()
+    const num = this.getNum();
+    const title = this.getTitle();
+    const source = this.getSource();
+    const language = this.getLanguage();
+    const sourceFilePath = this.getSourceFilePath();
+    const difficulty = this.getDifficulty();
 
-    return `|${num}|[${title}](${source})|[${language}](${sourceFilePath})|${difficulty}|\n`
+    return `|${num}|[${title}](${source})|[${language}](${sourceFilePath})|${difficulty}|\n`;
   }
 
   getNum() {
-    return this.title.split('.')[0].trim()
+    return this.title.split(".")[0].trim();
   }
 
   getTitle() {
-    return this.title.split('.')[1].trim()
+    return this.title.split(".")[1].trim();
   }
 
   getSource() {
-    const suffixes = ['/description', '/discuss', '/submissions']
+    const suffixes = ["/description", "/discuss", "/submissions"];
     return suffixes.some((suffix) => this.url.includes(suffix))
       ? this.url.slice(
           0,
-          Math.max(...suffixes.map((suffix) => this.url.indexOf(suffix))),
+          Math.max(...suffixes.map((suffix) => this.url.indexOf(suffix)))
         )
-      : this.url.replace(/\/$/, '')
+      : this.url.replace(/\/$/, "");
   }
 
   getLanguage() {
-    return LANG[this.getExtension()]
+    return LANG[this.getExtension()];
   }
 
   getExtension() {
-    return this.sourceFilePath.slice(this.sourceFilePath.lastIndexOf('.') + 1)
+    return this.sourceFilePath.slice(this.sourceFilePath.lastIndexOf(".") + 1);
   }
 
   getSourceFilePath() {
-    return this.sourceFilePath
+    return this.sourceFilePath;
   }
 
   getDifficulty() {
-    return this.difficulty.trim()
+    return this.difficulty.trim();
   }
 
   fetch(url) {
     return new Promise((resolve, reject) => {
       https
         .get(url, (resp) => {
-          let data = ''
+          let data = "";
 
-          resp.on('data', (chunk) => {
-            data += chunk
-          })
+          resp.on("data", (chunk) => {
+            data += chunk;
+          });
 
-          resp.on('end', () => {
-            resolve(JSON.parse(data))
-          })
+          resp.on("end", () => {
+            resolve(JSON.parse(data));
+          });
         })
-        .on('error', (err) => {
-          reject(err.message)
-        })
-    })
+        .on("error", (err) => {
+          reject(err.message);
+        });
+    });
   }
 }
